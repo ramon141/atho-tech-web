b5387401ecd83c54b5c5b2b71407e07b401f5f1d
ramon141 <ramonbarbosa123456789@gmail.com>
2022-11-02 23:53:38 -0300

    Corrindo o link da api, novamente

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from "./auth";
 
 const api = axios.create({
-    baseURL: 'http://atho-tech-web.herokuapp.com/',
+    baseURL: 'http://atho-tech-api.herokuapp.com/',
 });
 
 /* Aqui foi utilizado o interceptors do Axios, 
