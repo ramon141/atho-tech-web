0615eb9814c8d3d3b74c20eaeed0dd38b7b69210
Ramon Barbosa <141barbosa@gmail.com>
2023-02-15 18:58:36 +0000

    Update 7 files

--- src/components/Sidebar/ListItems.jsx
+++ src/components/Sidebar/ListItems.jsx
@@ -1,52 +1,53 @@
-import * as React from 'react';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import { useNavigate } from 'react-router-dom';
-import {
-    ROLE
-} from '../../services/auth';
+import * as React from "react";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { useNavigate } from "react-router-dom";
+import { ROLE } from "../../services/auth";
 
 export default function ListItems() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  return (
+    <List>
+      <Divider />
 
-    return (
-        <List>
-            <Divider />
+      <ListItem>
+        <ListItemButton onClick={() => navigate("/choice-kits")}>
+          <ListItemText primary={"Escolher Kit"} />
+        </ListItemButton>
+      </ListItem>
 
-            <ListItem>
-                <ListItemButton onClick={() => navigate('/choice-kits')}>
-                    <ListItemText primary={'Escolher Kit'} />
-                </ListItemButton>
-            </ListItem>
+      <ListItem>
+        <ListItemButton onClick={() => navigate("/calculate")}>
+          <ListItemText primary={"Calculadora"} />
+        </ListItemButton>
+      </ListItem>
 
-            <ListItem>
-                <ListItemButton onClick={() => navigate('/calculate')}>
-                    <ListItemText primary={'Calculadora'} />
-                </ListItemButton>
-            </ListItem>
+      {ROLE === "Admin" ? (
+        <>
+          <ListItem>
+            <ListItemButton onClick={() => navigate("/register-seller")}>
+              <ListItemText primary={"Cadastro de Vendedores"} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton onClick={() => navigate("/products-pricing")}>
+              <ListItemText primary={"Editar preço dos produtos"} />
+            </ListItemButton>
+          </ListItem>
 
-            {
-                ROLE === 'Admin' ?
-                    <ListItem>
-                        <ListItemButton onClick={() => navigate('/register-seller')}>
-                            <ListItemText primary={'Cadastro de Vendedores'} />
-                        </ListItemButton>
-                    </ListItem> : false
-            }
-
-            {
-                ROLE === 'Admin' ?
-                    <ListItem>
-                        <ListItemButton onClick={() => navigate('/products-pricing')}>
-                            <ListItemText primary={'Editar preço dos produtos'} />
-                        </ListItemButton>
-                    </ListItem> : false
-            }
-
-        </List>
-    );
+          <ListItem>
+            <ListItemButton onClick={() => navigate("/register-product")}>
+              <ListItemText primary={"Cadastrar Produto"} />
+            </ListItemButton>
+          </ListItem>
+        </>
+      ) : (
+        false
+      )}
+    </List>
+  );
 }
---
+++ src/pages/RegisterProduct/index.jsx
@@ -0,0 +1,119 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import api from "../../services/api";
+import { useNavigate } from "react-router-dom";
+import imgBackground from "../../assets/background.png";
+import { FaUnlock as LockIcon } from "react-icons/fa";
+import { login as addEnvVarsFromRequestLogin } from "../../services/auth";
+
+const classes = {
+  root: { margin: 30 },
+  title: { fontWeight: 600, marginBottom: 30 },
+  buttonSubmit: {
+    backgroundColor: "#1976D2",
+    color: "white",
+    borderColor: "rgba(0,0,0,0.50)",
+  },
+  buttonClean: {
+    backgroundColor: "#1976D2",
+    color: "white",
+    borderColor: "rgba(0,0,0,0.50)",
+  },
+};
+
+export default function RegisterProduct() {
+  const [value, setValue] = useState("");
+  const [description, setDescription] = useState("");
+
+  const register = async () => {
+    try {
+      const dataProducts = { description };
+      const { data: product } = await api.post("products", dataProducts);
+
+      const dataConfig = {
+        description: " ",
+        productsId: product.id,
+        value: parseFloat(value),
+      };
+
+      await api.post("configurations", dataConfig);
+
+      cleanFields();
+      alert("Produto cadastrado com sucesso!");
+    } catch (error) {
+      alert(
+        error?.response?.data?.error?.message || "Um erro desconhecido ocorreu"
+      );
+    }
+  };
+
+  const cleanFields = () => {
+    setValue("");
+    setDescription("");
+  };
+
+  return (
+    <Card style={classes.root}>
+      <CardContent>
+        <Typography variant="h4" style={classes.title} align="center">
+          Cadastro de Produtos
+        </Typography>
+
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <TextField
+              label="Nome"
+              fullWidth
+              size="small"
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
+            <TextField
+              label="Valor"
+              fullWidth
+              type="number"
+              size="small"
+              onChange={(e) => setValue(e.target.value)}
+              value={value}
+            />
+          </Grid>
+
+          <Grid container item justifyContent="center" spacing={2}>
+            <Grid item xs={12} sm={3} md={2.5}>
+              <Button
+                variant="outlined"
+                fullWidth
+                style={classes.buttonClean}
+                onClick={cleanFields}
+              >
+                Limpar
+              </Button>
+            </Grid>
+
+            <Grid item xs={12} sm={3} md={2.5}>
+              <Button
+                variant="outlined"
+                fullWidth
+                style={classes.buttonSubmit}
+                onClick={register}
+              >
+                Cadastrar
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+}
